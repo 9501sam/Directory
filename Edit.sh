4245f1a55527261df6cbcd14cfeae063030e508e
turtlegod <9501sam@gmail.com>
2020-12-27 03:27:37 +0800

    add class Person

--- Edit.sh
+++ Edit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-vim ~/Java_Project/Directory/src/directory/*.java -p
+vim ~/Java_Project/Directory/src/directory/*.java ~/Java_Project/Directory/src/directory/*/*.java -p
 
---
+++ src/directory/fileIO/Person.java
@@ -0,0 +1,66 @@
+package directory.fileIO
+
+public class Person{
+
+	private String name;
+	private int birthDay;
+	private String phoneNumber;
+	private String category;
+	private String E_mail;
+
+	public Person(){
+		name = "no name";
+		birthDay = "0101";
+		phoneNumber = "0000-000000";
+		category = "A";
+		EMail = "hahaha123@myserver.com";
+	}
+	
+	public Person(String n, String bir, String ph, String ca, String e){
+		name = n;
+		birthDay = bir;
+		phoneNumber = ph;
+		category = ca;
+		EMail = e;
+	}
+
+	public void setName(String str){
+		name = str;
+	}
+
+	public void setBirthDay(String str){
+		birthDay = str;
+	}
+
+	public void setPhoneNumber(String str){
+		phoneNumber = str;
+	}
+
+	public void setCategory(String str){
+		category = str;
+	}
+
+	public void setEMail(String str){
+		EMail = str;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getBirthDay(){
+		return birthDay;
+	}
+
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+
+	public String getCategory(){
+		return category;
+	}
+
+	public String getEMail(){
+		return EMail;
+	}
+}

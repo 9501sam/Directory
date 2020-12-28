13760dc09bc11c18d2febc4c19a0ba683af2ba85
turtlegod <9501sam@gmail.com>
2020-12-29 03:17:41 +0800

    Reader add readOneNumber function

--- src/directory/fileIO/Reader.java
+++ src/directory/fileIO/Reader.java
@@ -10,12 +10,13 @@ import directory.data.Data;
 
 public class Reader{
 
+	//for DataFrame to read Header
 	public static String[] readHeader(){
 		Scanner inputStream = null;
 		try{
 			inputStream = new Scanner(new FileInputStream("res/data.csv"));
 		}catch(FileNotFoundException e){
-			System.out.println("Error in directory.fileIO.CsvReader:");
+			System.out.println("Error in directory.fileIO.Reader:");
 			System.out.println("res/data.csv not found.");
 		}
 		String firstLineInFile = inputStream.nextLine();
@@ -23,12 +24,13 @@ public class Reader{
 		return header;
 	}
 
+	//for DataFrame to read AllData
 	public static ArrayList<Data> readCsvFile(){
 		Scanner inputStream = null;
 		try{
 			inputStream = new Scanner(new FileInputStream("res/data.csv"));
 		}catch(FileNotFoundException e){
-			System.out.println("Error in directory.fileIO.CsvReader:");
+			System.out.println("Error in directory.fileIO.Reader:");
 			System.out.println("res/data.csv not found.");
 		}
 		String header[] = inputStream.nextLine().split(",");
@@ -39,6 +41,20 @@ public class Reader{
 			allData.add(aData);
 		}
 		return allData;
+	}
+
+	//for Login to read authentication
+	public static int readOneNumber(){
+		Scanner inputStream = null;
+		try{
+			inputStream = new Scanner(new FileInputStream(
+						"res/setting/authentication"));
+		}catch(FileNotFoundException e){
+			System.out.println("Error in directory.fileIO.Reader:");
+			System.out.println("res/setting/authentication not found.");
+		}
+		int num = inputStream.nextInt();
+		return num;
 	}
 
 }

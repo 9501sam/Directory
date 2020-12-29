286776ac3262f2fbb0eba2c37d4f6e5a30b54c40
turtlegod <9501sam@gmail.com>
2020-12-29 15:59:19 +0800

    Login authentication function complete!!

--- src/directory/login/Login.java
+++ src/directory/login/Login.java
@@ -29,9 +29,9 @@ public class Login{
 			if(inputID.equals(ID) && inputPassword.equals(PASSWORD)){
 				login = true;
 				System.out.println("id and password correct.");
+				System.out.println("");
 			}else{
-				System.out.println("login faild.");
-				System.out.println();
+				System.out.println("Login faild.");
 			}
 		}
 
@@ -42,30 +42,59 @@ public class Login{
 		while(!authen){
 			if(question == 0){
 				authen = true;
-			}
-			if(question == 1){ //ask user to sort three number between 1-100
+			}else if(question == 1){ //ask user to sort three number between 1-100
 				int num[] = new int[3];
 				for(int i = 0; i < 3; i++)
 					num[i] = rand.nextInt(100) + 1;
 				Arrays.sort(num);
-				System.out.println("Please sort the three number in non-decrease order:");
-				for(int i = 0; i < 3; i++)
-					System.out.print(num[i] + " ");
-				System.out.println("");
+				System.out.print("Please sort [");
+				for(int i = 0; i < 3; i++){
+					System.out.print(num[i]);
+					if(i != 3-1)
+						System.out.print(" ");
+				}
+				System.out.println("] in non-decrease order for authentication.");
+				System.out.print(":");
 				int input[] = new int[3];
 				for(int i = 0; i < 3; i++)
 					input[i] = keyboard.nextInt();
 				authen = true;
 				for(int i = 0; i < 3; i++)
-					if(input[i] != num[i])
+					if(input[i] != num[i]){
 						authen = false;
-			}else if(question == 2){
+						break;
+					}
+				if(authen == false)
+					System.out.println("Authentication failed.");
+			}else if(question == 2){ //ask user compute x [+/-/*] y = ??
+				int num1 = rand.nextInt(10) + 1;
+				int num2 = rand.nextInt(10) + 1;
+				int op = rand.nextInt(3);
+				int ans = 0;
+				System.out.print("Answer following question for authentication: ");
+				if(op == 0){ //"+"
+					ans = num1 + num2;
+					System.out.print(num1 + " + " + num2 + " = ");
+				}else if(op == 1){ //"-"
+					ans = num1 - num2;
+					System.out.print(num1 + " - " + num2 + " = ");
+				}else if(op == 2){ //"*"
+					ans = num1 * num2;
+					System.out.print(num1 + " * " + num2 + " = ");
+				}
+				int input = keyboard.nextInt();
+				if(input == ans)
+					authen = true;
+				else
+					System.out.println("Authentication failed.");
 			}else{
 				System.out.println(
 						"There is something wrong with res/setting/authentication");
 				System.exit(0);
 			}
 		}
-
+		if(question != 0)
+			System.out.println("Authentication success.");
+		System.out.println("Login success.");
 	}
 }
